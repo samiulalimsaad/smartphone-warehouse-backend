c57694ccedd014678ab09cb7c5df7f48f358ad88
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-01 22:33:10 +0600

    solved cors issue

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
@@ -6,7 +7,10 @@ import { Inventory } from "./Model/inventory.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//  middleWares
+app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.json({ hello: "hello" });
