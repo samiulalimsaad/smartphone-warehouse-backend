f7a7cb54d7641fe162a0ac6a5dc2dfa3d731407d
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-01 18:05:27 +0600

    inventory model added

---
+++ Model/inventory.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { model, Schema } = mongoose;
+const inventorySchema = new Schema(
+    {
+        name: String,
+        images: [String],
+        quantity: Number,
+        price: Number,
+        supplierName: String,
+        description: [String],
+        brandName: String,
+    },
+    { timestamps: true }
+);
+
+export const Inventory = model("inventory", inventorySchema);
