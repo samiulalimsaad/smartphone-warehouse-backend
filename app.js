ef33329d7a562508f8253ea096ca15c3e6a30001
samiulalimsaad <saadnbiuraj@gmail.com>
2022-05-03 19:37:26 +0600

    cors issue 1

--- app.js
+++ app.js
@@ -4,26 +4,14 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import { Inventory } from "./Model/inventory.js";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //  middleWares
 // enable cors
-app.use(
-    cors({
-        origin: true,
-        optionsSuccessStatus: 200,
-        credentials: true,
-    })
-);
-app.options(
-    "*",
-    cors({
-        origin: true,
-        optionsSuccessStatus: 200,
-        credentials: true,
-    })
-);
+app.use(cors());
+
 app.use(express.json());
 
 const verifyUser = (req, res, next) => {
